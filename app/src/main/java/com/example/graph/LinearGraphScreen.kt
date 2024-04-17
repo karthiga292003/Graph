e3259f320738182c0c7f9084352a9608d19a0901
karthiga Murugan <karthiga.m.2021.ece@ritchennai.edu.in>
2024-04-17 10:12:28 +0530

    Initial commit

---
+++ app/src/main/java/com/example/graph/LinearGraphScreen.kt
@@ -0,0 +1,102 @@
+package com.example.graph
+
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import co.yml.charts.axis.AxisData
+import co.yml.charts.common.model.Point
+import co.yml.charts.ui.linechart.LineChart
+import co.yml.charts.ui.linechart.model.GridLines
+import co.yml.charts.ui.linechart.model.IntersectionPoint
+import co.yml.charts.ui.linechart.model.Line
+import co.yml.charts.ui.linechart.model.LineChartData
+import co.yml.charts.ui.linechart.model.LinePlotData
+import co.yml.charts.ui.linechart.model.LineStyle
+import co.yml.charts.ui.linechart.model.LineType
+import co.yml.charts.ui.linechart.model.SelectionHighlightPoint
+import co.yml.charts.ui.linechart.model.SelectionHighlightPopUp
+import co.yml.charts.ui.linechart.model.ShadowUnderLine
+
+@Composable
+fun LinearGraph() {
+    val steps = 10
+    var pointsData:List< Point> = listOf(
+            Point(0f, 20f),
+            Point(1f, 80f),
+            Point(2f, 40f),
+            Point(3f, 70f),
+            Point(4f, 40f)
+        )
+
+        val xAxisData = AxisData.Builder()
+            .axisStepSize(100.dp)
+            .backgroundColor(Color.LightGray)
+            .steps(pointsData.size - 1)
+            .labelData { i -> i.toString() }
+            .labelAndAxisLinePadding(15.dp)
+            .build()
+
+        val yAxisData = AxisData.Builder()
+            .steps(steps)
+            .backgroundColor(Color.Gray)
+            .labelAndAxisLinePadding(20.dp)
+            .labelData { i ->
+                val yScale = 100 / steps
+                (i * yScale).toString()
+            }
+            .axisLineColor(MaterialTheme.colorScheme.secondary)
+            .axisLabelColor(MaterialTheme.colorScheme.tertiary)
+            .build()
+
+        val lineChartData = LineChartData(
+            linePlotData = LinePlotData(
+                lines = listOf(
+                    Line(
+                        dataPoints = pointsData,
+                        LineStyle(
+                            color = MaterialTheme.colorScheme.tertiary,
+                            lineType = LineType.SmoothCurve(isDotted = false)
+                        ),
+                        IntersectionPoint(
+                            color = MaterialTheme.colorScheme.tertiary
+                        ),
+                        SelectionHighlightPoint(),
+                        ShadowUnderLine(
+                            alpha = 0.5f,
+                            brush = Brush.verticalGradient(
+                                colors = listOf(
+                                    MaterialTheme.colorScheme.inversePrimary,
+                                    Color.Transparent
+                                )
+                            )
+                        ),
+                        SelectionHighlightPopUp()
+                    )
+                ),
+            ),
+            xAxisData = xAxisData,
+            yAxisData = yAxisData,
+            gridLines = GridLines(),
+            backgroundColor = MaterialTheme.colorScheme.background
+        )
+        LineChart(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(300.dp),
+            lineChartData = lineChartData
+        )
+    }
+
+@Preview
+@Composable
+fun GreetingPreview() {
+    LinearGraph()
+}
